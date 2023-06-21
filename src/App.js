55e7aed22a93e758937024305184918efd90deb2
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-06-20 18:00:38 -0700

    Removed logo, organized app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
+
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return <div className="App">Weather App</div>;
 }
-
-export default App;
