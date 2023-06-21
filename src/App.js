cba445a8f36bc95d917c6d495d82db10113f8b91
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-06-20 18:21:27 -0700

    Added bootstrap and fixed app.js code with github link

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Cristina Camacho and is
-        <a href="https://github.com/camachoc915/Weather-react" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Cristina Camacho and is
+          <a
+            href="https://github.com/camachoc915/Weather-react"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
