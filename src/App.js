d4102c38e8d64eaeac0e12a1266dadd070902065
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-06-20 18:06:45 -0700

    Added github open-source details and fixed app.js

--- src/App.js
+++ src/App.js
@@ -4,5 +4,15 @@ import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Cristina Camacho and is
+        <a href="https://github.com/camachoc915/Weather-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
