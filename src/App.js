5f5ad987d4f0042e3c0825597d4509f38f5bc50e
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-06-22 18:41:37 -0700

    adjusted city function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="San Diego" />
         <footer>
           This project was coded by Cristina Camacho and is
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -122,8 +122,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "4d47aca4t54edb1e29ed0ff22da3a8do";
-    let city = "New York";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
